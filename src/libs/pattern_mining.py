ffb5d05c6306d56b9bd9802f5e37c855d7114809
TheExGenesis <joao.ag.andrade@gmail.com>
2019-10-31 15:33:20 +0000

    patterm mining

--- src/libs/pattern_mining.py
+++ src/libs/pattern_mining.py
@@ -4,6 +4,7 @@ from IPython.display import display, HTML
 from sklearn.preprocessing import LabelBinarizer #for dummification
 from mlxtend.frequent_patterns import apriori, association_rules #for ARM
 from sklearn.feature_selection import SelectKBest, f_classif
+from treatment import discretize, dummify
 
 
 #datapath = '../data/data/proj/pd_speech_features.csv'
@@ -11,25 +12,6 @@ from sklearn.feature_selection import SelectKBest, f_classif
 #df = speechdata 
 #print(df.head())
 
-
-#Discretize real-valued attributes: width (pd.cut)
-def discretize_width(df):
-    newdf = df.copy()
-    for col in newdf:
-        if col not in ['class','id','gender']: 
-            newdf[col] = pd.cut(newdf[col],3,labels=['0','1','2'])
-    print(newdf.head(5))
-    return newdf
-
-
-#Discretize real-valued attributes: depth (pd.qcut)
-def discretize_depth(df):
-    newdf = df.copy()
-    for col in newdf:
-        if col not in ['class','id','gender']: 
-            newdf[col] = pd.qcut(newdf[col],3,labels=['0','1','2'])
-    print(newdf.head(5))
-    return newdf
 
 #Class notes
 # Cross validation can't be done with the python package, because we need to preprocess the training data only after taking the test set
@@ -39,38 +21,25 @@ def discretize_depth(df):
 #xgboost melhor que bagging do sklearn
 #
 
-# dummify
-def dummify(newdf):
-    dummylist = []
-    for att in newdf:
-        if att in ['class','id','gender']: newdf[att] = newdf[att].astype('category')
-        dummylist.append(pd.get_dummies(newdf[[att]]))
-    dummified_df = pd.concat(dummylist, axis=1)
-    print(dummified_df.head(5))
-    return dummified_df
 
-def get_frequent_itemsets(dummified_df):
-    minsup = 0.35 #you can also use iteratively decreasing support as in the previous example
-    frequent_itemsets = apriori(dummified_df, min_support=minsup, use_colnames=True)
-    print(frequent_itemsets)
+def get_frequent_itemsets(dummified_df, minsup = 0.35, iteratively_decreasing_support = False, minpatterns = 30):
+    frequent_itemsets = {}
+    if iteratively_decreasing_support:
+        while minsup > 0:
+            minsup = minsup*0.9
+            frequent_itemsets = apriori(dummified_df, min_support=minsup, use_colnames=True)
+        if len(frequent_itemsets) >= minpatterns:
+            print("Minimum support:",minsup)
+            break
+    else:
+        frequent_itemsets = apriori(dummified_df, min_support=minsup, use_colnames=True)
+    print("Number of found patterns:",len(frequent_itemsets))
     return frequent_itemsets
 
-def get_association_rules(frequent_itemsets):
+#metric can also be lift
+def get_association_rules(frequent_itemsets, metric="confidence", min_conf = 0.7, min_lift=1.2):
     minconf = 0.7
     rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=minconf)
     rules["antecedent_len"] = rules["antecedents"].apply(lambda x: len(x))
     print(rules[(rules['antecedent_len']>=2)])
     return rules
-
-"""
-columns = SelectKBest(f_classif, k=10).fit(x, y).get_support()
-new_x = x.loc[:,columns]
-dummify(discretize(new_x))
-
-#dw = discretize_width(df)
-#dummies = dummify(dw)
-#freqs = get_frequent_itemsets(dummies)
-#get_association_rules(freqs)
-
-patternMining(df)
-"""
